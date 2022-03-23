2bf5d0c7620fcc7ee8b648bd15cc836f9c05c2c1
Finnral <muhamadrizal2nd@gmail.com>
2022-03-23 14:01:28 +0800

    calls data form data.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import data from "./albums/data.js";
 
 function App() {
+  console.log(data.album.artists[0].external_urls);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Card">
+          <ul>
+            <li>{data.album.type}</li>
+            <li>
+              <h3>{data.album.name}</h3>
+            </li>
+            <li>
+              <a
+                className="App-link"
+                href={data.album.artists[0].external_urls.spotify}
+              >
+                {data.album.artists[0].name}
+              </a>{" "}
+              . {data.album.release_date} . {data.album.total_tracks} Lagu
+            </li>
+          </ul>
+          <img src={data.album.images[0].url} className="App-logo" alt="logo" />
+          <br></br>
+
+          <a href={data.album.external_urls.spotify}>
+            <button className="btn">Album</button>
+          </a>
+          <a href={data.external_urls.spotify}>
+            <button className="btn lirik">Lyric</button>
+          </a>
+        </div>
       </header>
     </div>
   );
